deaffdb66c3770ee0fd326a7ae4348ba4b002a4b
Alex Rael <alprael22@gmail.com>
2018-10-18 11:36:28 -0600

    created jar file and added styles

--- src/edu/cnm/deepdive/craps/Main.java
+++ src/edu/cnm/deepdive/craps/Main.java
@@ -1,18 +1,26 @@
 package edu.cnm.deepdive.craps;
 
+import edu.cnm.deepdive.craps.controller.Controller;
 import java.io.IOException;
 import java.util.ResourceBundle;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+  private static final String RESOURCE_PATH = "res";
+  private static final String ICON_PNG = RESOURCE_PATH + "/icon.png";
+  public static final String UI_PATH_BUNDLE = RESOURCE_PATH + "/ui";
+  public static final String WINDOW_TITLE_KEY = "window_title";
+  private static final String MAIN_PATH_STRING = RESOURCE_PATH + "/main.fxml";
   private ClassLoader classLoader;
   private ResourceBundle bundle;
   private FXMLLoader fxmlLoader;
+  private Controller controller;
 
   public static void main(String[] args) {
     launch(args);
@@ -24,22 +32,28 @@ public class Main extends Application {
     setupStage(stage, loadLayout());
   }
 
+  @Override
+  public void stop() throws Exception {
+    controller.stop();
+    super.stop();
+  }
+
   private void setupLoaders() {
     classLoader = getClass().getClassLoader();
-    bundle = ResourceBundle.getBundle("res/ui");
-    fxmlLoader = new FXMLLoader(classLoader.getResource("res/main.fxml"), bundle);
+    bundle = ResourceBundle.getBundle(UI_PATH_BUNDLE);
+    fxmlLoader = new FXMLLoader(classLoader.getResource(MAIN_PATH_STRING), bundle);
   }
 
   private Parent loadLayout() throws IOException {
     Parent root = fxmlLoader.load();
-    // TODO Do something more?
+    controller = fxmlLoader.getController();
     return root;
   }
 
   private void setupStage(Stage stage, Parent root) {
     Scene scene = new Scene(root);
-    stage.setTitle(bundle.getString("window_title"));
-    // TODO Set icon, etc.
+    stage.setTitle(bundle.getString(WINDOW_TITLE_KEY));
+    stage.getIcons().add(new Image(classLoader.getResourceAsStream(ICON_PNG)));
     stage.setResizable(false);
     stage.setScene(scene);
     stage.show();
--- src/edu/cnm/deepdive/craps/controller/Controller.java
+++ src/edu/cnm/deepdive/craps/controller/Controller.java
@@ -2,8 +2,12 @@ package edu.cnm.deepdive.craps.controller;
 
 import edu.cnm.deepdive.craps.model.Game;
 import edu.cnm.deepdive.craps.model.Game.Roll;
+import edu.cnm.deepdive.craps.model.Game.State;
+import edu.cnm.deepdive.craps.view.RollCell;
 import java.security.SecureRandom;
 import java.util.List;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
@@ -12,12 +16,17 @@ import javafx.scene.text.Text;
 
 public class Controller {
 
+  public static final String PLACEHOLDER_CLASS = "";
+  public static final String WIN_STYLE_CLASS = "win";
+  public static final String LOSS_STYLE_CLASS = "loss";
   private Game game;
   private boolean running;
   private String tallyFormat;
 
   @FXML
-  private ListView rolls;
+  private ResourceBundle resources;
+  @FXML
+  private ListView<Roll> rolls;
   @FXML
   private Text tally;
   @FXML
@@ -33,7 +42,10 @@ public class Controller {
   @FXML
   private void initialize() {
     tallyFormat = tally.getText();
+    rolls.setCellFactory((lv) -> new RollCell(resources));
+    rolls.getStyleClass().add(PLACEHOLDER_CLASS);
     reset(null);
+    updateMenu();
   }
 
   @FXML
@@ -46,8 +58,8 @@ public class Controller {
   @FXML
   private void fast(ActionEvent actionEvent) {
     running = true;
-    // TODO update menu items enabled/disabled
-    // TODO Instantiate and start Runner
+    updateMenu();
+    new Runner().start();
   }
 
   @FXML
@@ -63,13 +75,68 @@ public class Controller {
     updateRolls(game.getRolls());
   }
 
-  private void updateTally (int wins, int losses) {
-   int total = wins + losses;
-   double percentage = (total != 0) ? (100.0 * wins / total) : 0;
-   tally.setText(String.format(tallyFormat, wins, total, percentage));
+  public void stop() {
+    pause(null);
   }
 
-  private void updateRolls (List<Roll> rolls) {
-  // TODO Rolls ListView
+  private void updateTally(int wins, int losses) {
+    int total = wins + losses;
+    double percentage = (total != 0) ? (100.0 * wins / total) : 0;
+    tally.setText(String.format(tallyFormat, wins, total, percentage));
+  }
+
+  private void updateRolls(List<Roll> diceRolls) {
+    List<String> styleClasses = rolls.getStyleClass();
+    styleClasses.remove(styleClasses.size() - 1);
+    rolls.getItems().clear();
+    if (!diceRolls.isEmpty()) {
+      State state = diceRolls.get(diceRolls.size() - 1).getState();
+      if (state == State.WIN) {
+        styleClasses.add(WIN_STYLE_CLASS);
+      } else {
+        styleClasses.add(LOSS_STYLE_CLASS);
+      }
+      rolls.getItems().addAll(diceRolls);
+    } else {
+      styleClasses.add(PLACEHOLDER_CLASS);
+    }
+  }
+
+  private void updateMenu() {
+    next.setDisable(running);
+    fast.setDisable(running);
+    pause.setDisable(!running);
+    reset.setDisable(running);
+  }
+
+  private class Runner extends Thread {
+    private static final int TALLY_UPDATE_INTERVAL = 2_000;
+    private static final int ROLLS_UPDATE_INTERVAL = 10_000;
+
+    @Override
+    public void run() {
+      int count = 0;
+      while (running) {
+        game.play();
+        count++;
+        if (count % TALLY_UPDATE_INTERVAL == 0) {
+          int wins = game.getWins();
+          int losses = game.getLosses();
+          Platform.runLater(() -> updateTally(wins, losses));
+        }
+        if (count % ROLLS_UPDATE_INTERVAL == 0) {
+          List<Roll> rolls = game.getRolls();
+          Platform.runLater(() -> updateRolls(rolls));
+        }
+      }
+      int wins = game.getWins();
+      int losses = game.getLosses();
+      List<Roll> rolls = game.getRolls();
+      Platform.runLater(() -> {
+        updateTally(wins, losses);
+        updateRolls(rolls);
+        updateMenu();
+      });
+    }
   }
 }
